6837af2d097f06dd0438368bb568b7dad4504632
tehhanyi <hanyi.teh@beveat.com>
2024-11-10 15:23:10 +0800

    Update code to login from session (when logged in into firefox)

--- find_unfollowers.py
+++ find_unfollowers.py
@@ -1,40 +1,47 @@
 import instaloader
-import getpass
+from firefox_session import loginFromSession
 
-loader = instaloader.Instaloader()
+def find_unfollowers(loader, username):
+    profile = instaloader.Profile.from_username(loader.context, username)
 
-username = input("Username: ")
-password = getpass.getpass("Password: ")
-try:
-    loader.login(username, password)
-except instaloader.TwoFactorAuthRequiredException:
-    two_factor_code = input("Enter the 2FA code: ")
-    loader.two_factor_login(two_factor_code)
-# except instaloader.WrongPasswordException:
-#     print("Wrong password")
-#     password = input("Password: ")
-#     L.login(username, password)
+    print ("loading followers...")
+    followers = profile.get_followers()
+    print ("now loading followees...")
+    followings = profile.get_followees()
 
-profile = instaloader.Profile.from_username(loader.context, username)
+    followers_set = {follower.username for follower in followers}
+    print("Total followers:", len(followers_set))
+    followings_set = {following.username for following in followings}
+    print("Total followee:", len(followings_set))
 
-print ("loading followers...")
-followers = profile.get_followers()
-print ("now loading followees...")
-followings = profile.get_followees()
+    if username == "tehhanyi":
+        with open('influencers.txt', 'r') as file:
+            influencers = {line.strip() for line in file}
+    else:
+        influencers = set()
 
-followers_set = {follower.username for follower in followers}
-print("Total followers:", len(followers_set))
-followings_set = {following.username for following in followings}
-print("Total followee:", len(followings_set))
+    non_follow_back = followings_set - followers_set - influencers
+    print("\nUsers who don't follow you back:")
+    for user in non_follow_back:
+        print(user)
 
-if username == "tehhanyi":
-    with open('influencers.txt', 'r') as file:
-        influencers = {line.strip() for line in file}
-else:
-    influencers = set()
+def loginWithCredentials():
+    import getpass
 
-non_follow_back = followings_set - followers_set - influencers
-print("\nUsers who don't follow you back:")
-for user in non_follow_back:
-    print(user)
+    loader = instaloader.Instaloader()
 
+    username = input("Username: ")
+    password = getpass.getpass("Password: ")
+    try:
+        loader.login(username, password)
+    except instaloader.TwoFactorAuthRequiredException:
+        two_factor_code = input("Enter the 2FA code: ")
+        loader.two_factor_login(two_factor_code)
+    except instaloader.WrongPasswordException:
+        print("Wrong password")
+        password = input("Password: ")
+        loader.login(username, password)
+
+if __name__ == "__main__":
+    loader, username = loginFromSession() #loginWithCredentials() when instaloader.exceptions.BadCredentialsException fixed
+    find_unfollowers(loader, username)
